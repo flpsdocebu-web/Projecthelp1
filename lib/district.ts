eb4b19c2b104a70dd9494f8654a9a241db019222
danielreyes01-jpg <daniel.reyes01@deped.gov.ph>
2026-08-04 08:53:17 +0800

    Correct Alcantara district spelling

--- lib/district.ts
+++ lib/district.ts
@@ -5,6 +5,7 @@ export function normalizedDistrict(value?: string) {
     .replace(/([A-Z])([0-9])$/g, "$1 $2")
     .replace(/\s+/g, " ");
   key = key.replace(/\s+II$/i, " 2").replace(/\s+I$/i, " 1");
+  if (key === "ALCANATARA") key = "ALCANTARA";
   const label = key.toLowerCase().replace(/(^|\s)([a-z])/g, (_, space, letter) => `${space}${letter.toUpperCase()}`);
   return { key, label };
 }
